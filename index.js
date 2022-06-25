06fe11f14e4fdb1ee123afd17e94a80e5bb710af
JOY <joy.kemunto@student.moringaschool.com>
2022-06-25 20:48:42 +0300

    commit tree 6931efa6a0eb1d3dda61a3b6946072baeead0225

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code here
+//lets create a class breakfast
+
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food
+      this.drink = drink
+    }
+  }
+
+//lets create a class lunch
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+//lets create a class dinner
+class Dinner {
+    constructor(salad, soup, entree){
+    this.salad = salad
+    this.soup = soup
+    this.entree = entree
+}
+}
+
+
+
